8769211e68fd0e0f78e866a9664a9ba7abd2425b
shabbiryusufali <shabyusufali@gmail.com>
2022-10-15 14:34:54 -0700

    make a proper frontend

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,14 +6,16 @@ import Lot from './components/Lot';
 import Login from './components/Login'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Signup from "./components/Signup";
+import Header from './components/Header';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-
     <BrowserRouter>
+    <Header />
+    <br />
     <Routes>
       <Route path='/lot' element={<Lot />} />
       <Route path='/login' element={<Login />} />
