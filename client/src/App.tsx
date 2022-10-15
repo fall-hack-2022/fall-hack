41aa66e0bf5241a87bb1a649c045bbad6e0c9c5a
shabbiryusufali <shabyusufali@gmail.com>
2022-10-15 11:19:16 -0700

    add router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Link} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <Link to='/lot'>Lot</Link>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and SAVE to reload.
         </p>
         <a
           className="App-link"
---
+++ client/src/components/Lot.tsx
@@ -0,0 +1,10 @@
+
+function Lot() {
+  return (
+    <div>
+        <div>ASJDFK:SDAJGK:LADWSJGLK:</div>
+    </div>
+  )
+}
+
+export default Lot
